e464df330130a65375e6b1b1e2f5051a60261b8d
Nishant Varshney <nishant.varshney@soti.net>
2020-04-13 20:07:15 +0530

    Added server side implementation of photo upload

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using AutoMapper;
+using Infrastructure.Photos;
 
 namespace API
 {
@@ -88,6 +89,8 @@ namespace API
             });
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Application/Users/Register.cs
+++ Application/Users/Register.cs
@@ -36,7 +36,7 @@ namespace Application.Users
             }
 
         }
-        public class Handler : IRequestHandler<Command,User>
+        public class Handler : IRequestHandler<Command, User>
         {
             private readonly DataContext _context;
             private readonly UserManager<AppUser> _userManager;
@@ -48,13 +48,13 @@ namespace Application.Users
                 _context = context;
             }
 
-           public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
             {
                 if (await _context.Users.Where(x => x.Email == request.Email).AnyAsync())
-                    throw new RestException(HttpStatusCode.BadRequest, new {Email = "Email already exists"});
+                    throw new RestException(HttpStatusCode.BadRequest, new { Email = "Email already exists" });
 
                 if (await _context.Users.Where(x => x.UserName == request.Username).AnyAsync())
-                    throw new RestException(HttpStatusCode.BadRequest, new {Username = "Username already exists"});
+                    throw new RestException(HttpStatusCode.BadRequest, new { Username = "Username already exists" });
 
                 var user = new AppUser
                 {
@@ -72,7 +72,7 @@ namespace Application.Users
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
                         Username = user.UserName,
-                        Image = null
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
                 }
 
